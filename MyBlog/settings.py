a97917c53c5ed76d8934bb74f9dcdc57213c8129
Sidixs <104302571+Sidixs@users.noreply.github.com>
2022-05-26 15:25:59 +0200

    Update settings.py

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ev7x@aq2(ls5hlpq4*x=8_evw+(v=932%gp6-_9c6pm=os=tbd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mojblog.azurewebsites.net']
 
 
 # Application definition
