20a9819ddf90724ac6da35c453c21c3823fb922a
FT <ft6180@gmail.com>
2022-06-02 19:46:56 +0200

    Revert "URL dla profili"
    This reverts commit fdfbb27b7f4e5b3fb661514117a6d2acd40657a6.

--- authentication/urls.py
+++ authentication/urls.py
@@ -9,7 +9,7 @@ urlpatterns =[
     path('signup', views.view_user.sign_up, name='signup'),
     path('create-post', views.view_post.create_post, name='create-post'),
     path('tos', views.view_site_misc.TOS_page, name='tos'),
-    path('profile/<str:userid>', views.view_user.profile, name='profile'),
+    path('profile', views.view_user.profile, name='profile'),
     path('followers', views.view_follower.followers_page, name='followers'),
     path('edit-profile', views.view_user.edit_profile, name='edit-profile'),
 ]
--- authentication/views/view_user.py
+++ authentication/views/view_user.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from ..forms import RegisterForm, PostForm, EditProfileForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
-from ..models import AuthUser, AuthenticationPost
+from ..models import AuthenticationPost
 from ..models import Post
 from ..models import AccBlogSettings
 from ..models import AuthUser
@@ -28,20 +28,18 @@ def sign_up(request):
 
 
 @login_required(login_url="/login")
-def profile(request, userid):
-    user = AuthUser.objects.get(id=userid)
+def profile(request):
     posts_profile = AuthenticationPost.objects.filter(author=request.user.id)
     if request.method == "POST":
         post_id = request.POST.get("post-id")
         delete_post = AuthenticationPost.objects.filter(id=post_id)
         if posts_profile:# and delete_post.author == request.user:
             delete_post.delete()
-            return render(request, 'profile.html')
+            return redirect('/profile')
 
     # else:
     #     posts_profile = PostForm()
-    context= {'user':user, 'posts_profile':posts_profile}
-    return render(request, 'profile.html', context)
+    return render(request, 'profile.html', {'posts_profile': posts_profile})
 
 
 @login_required(login_url="/login")
