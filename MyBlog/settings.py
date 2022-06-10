d56e575d3dceaa7d3b7d5f6ab449c0dac7f617e4
Jakub Jurek <jurek.kuba11@gmail.com>
2022-06-10 15:56:13 +0200

    dodanie opcji zresetowania hasla poprzez podanie adresu email

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -154,3 +154,12 @@ CKEDITOR_CONFIGS = {
         #'resize_maxWidth': '150%',
     },
 }
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'myblogservice00@gmail.com'
+EMAIL_HOST_PASSWORD = 'fasijukgufibstey'
