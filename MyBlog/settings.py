b4728ad8a8aac14ba44122dbe52e2100f7706046
Sidixs <sylwesterkowal2000@gmail.com>
2022-06-15 19:09:37 +0200

    Update settings.py

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'MyBlog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'iodb_test_sylwester',
-        'USER': 'django_user_sylwester',
-        'PASSWORD': 'jxxAD7e4HTQZiFe',
+        'NAME': 'iodb_test_ogolny',
+        'USER': 'django_user_test_ogolny',
+        'PASSWORD': 'Atcc9VaDPJMnphs',
         'HOST': 'io-db-2022-pro-myblog.mysql.database.azure.com',
         'PORT': '3306'
     }
